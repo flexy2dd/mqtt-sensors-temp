0fb9d5a9e7e5398a3ee28a736a90d21385c2dfd5
root <root@piscine.(none)>
2018-05-12 12:14:42 +0200

    First commit

---
+++ mqtt-sensors-temp-publish.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import ConfigParser
+import subprocess
+from time import localtime, strftime
+import json
+import paho.mqtt.client as mqtt
+
+#
+# Global variables
+#
+config = ConfigParser.ConfigParser()
+config.read('/opt/mqtt-sensors-temp/conf')
+
+responseTopic = 'sensors/swimmingpool/response/'
+
+def getPoolTemp():
+    with open ('/var/1w_files/28-0000066e633b', "r") as poolFile:
+        poolData = poolFile.read()
+
+    return float(poolData[20:])/1000
+
+def getOutdoorTemp():
+    with open ('/var/1w_files/28-0000066f5a83', "r") as outdoorFile:
+        outdoorData = outdoorFile.read()
+
+    return float(outdoorData[20:])/1000
+
+#
+# Callback that is executed when the client receives a CONNACK response from the server.
+#
+def onConnect(client, userdata, flags, rc):
+   print("Connected with result code " + str(rc))
+
+#
+# Callback that is executed when we disconnect from the broker.
+#
+def onDisconnect(client, userdata, message):
+    print("Disconnected from the broker.")
+
+#-----------------------------------------------------------------------------
+# Main
+#-----------------------------------------------------------------------------
+
+# Call 1Wire for get temp
+subprocess.call("/opt/mqtt-sensors-temp/w1-read-temp.sh")
+
+# Create MQTT client instance
+mqttc = mqtt.Client(client_id=config.get('MQTT', 'clientID'), clean_session=True)
+
+mqttc.on_connect = onConnect
+mqttc.on_disconnect = onDisconnect
+
+# Connect to the broker
+mqttc.username_pw_set(config.get('MQTT', 'user'), password=config.get('MQTT', 'passwd'))
+mqttc.connect(config.get('MQTT', 'host'), port=int(config.get('MQTT', 'port')), keepalive=60, bind_address="")
+
+mqttc.loop_start()
+
+infoOutdoor = mqttc.publish((responseTopic + 'temp-outdoor'), payload=getOutdoorTemp(), qos=0, retain=True)
+infoOutdoor.wait_for_publish()
+
+infoPool = mqttc.publish((responseTopic + 'temp-pool'), payload=getPoolTemp(), qos=0, retain=True)
+infoPool.wait_for_publish()
+
+# End
---
+++ mqtt-sensors-temp-server.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import ConfigParser
+from time import localtime, strftime
+import json
+import paho.mqtt.client as mqtt
+
+#
+# Global variables
+#
+config = ConfigParser.ConfigParser()
+config.read('conf')
+
+requestTopic  = 'sensors/swimmingpool/+'        # Request comes in here. Note wildcard.
+responseTopic = 'sensors/swimmingpool/response/'        # Response goes here. Request ID will be appended later
+
+def getPoolTemp():
+    with open ('/var/1w_files/28-0000066e633b', "r") as poolFile:
+        poolData = poolFile.read()
+
+    return float(poolData[20:])/1000
+
+def getOutdoorTemp():
+    with open ('/var/1w_files/28-0000066f5a83', "r") as outdoorFile:
+        outdoorData = outdoorFile.read()
+
+    return float(outdoorData[20:])/1000
+
+#
+# Callback that is executed when the client receives a CONNACK response from the server.
+#
+def onConnect(client, userdata, flags, rc):
+   print("Connected with result code " + str(rc))
+
+   # Subscribe on request topic with a single-level wildcard.
+   # Subscribing in on_connect() means that if we lose the connection and
+   # reconnect then subscriptions will be renewed.
+   client.subscribe(requestTopic, 0)    # topic, QoS
+
+#
+# Callback that is executed when a message is received.
+#
+def onMessage(client, userdata, message):
+   requestTopic = message.topic
+
+   requestID = requestTopic.split('/')[2]
+
+   print("Received a time request on topic " + requestTopic + ".")
+
+   if requestID=='temp-pool':
+       client.publish((responseTopic + requestID), payload=getPoolTemp(), qos=0, retain=False) # Publish the time to the response topic
+
+   if requestID=='temp-outdoor':
+       client.publish((responseTopic + requestID), payload=getOutdoorTemp(), qos=0, retain=False) # Publish the time to the response topic
+
+#
+# Callback that is executed when we disconnect from the broker.
+#
+def onDisconnect(client, userdata, message):
+    print("Disconnected from the broker.")
+
+#-----------------------------------------------------------------------------
+# Main
+#-----------------------------------------------------------------------------
+
+# Create MQTT client instance
+mqttc = mqtt.Client(client_id=config.get('MQTT', 'clientID'), clean_session=True)
+
+mqttc.on_connect = onConnect
+mqttc.on_message = onMessage
+mqttc.on_disconnect = onDisconnect
+
+# Connect to the broker
+mqttc.username_pw_set(config.get('MQTT', 'user'), password=config.get('MQTT', 'passwd'))
+mqttc.connect(config.get('MQTT', 'host'), port=int(config.get('MQTT', 'port', 1883)), keepalive=60, bind_address="")
+
+# This is a blocking form of the network loop and will not return until the client
+# calls disconnect(). It automatically handles reconnecting.
+mqttc.loop_forever()
+
+# End
